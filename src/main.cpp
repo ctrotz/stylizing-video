bf5b984b473cc090708a7826bf0ea88e5720b3cb
ctrotz <caleb_trotz@brown.edu>
2020-04-17 01:36:25 -0400

    premerge

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,12 @@
 #include <Eigen/Core>
 
 #include "iohandler.h"
+#include "optical-flow/simpleflow.h"
+#include "opencvutils.h"
+
+
 
 using namespace std;
-
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
@@ -57,7 +60,34 @@ int main(int argc, char *argv[])
 
     ioHandler.loadInputData(inputFrames, keyframes);
 
-    ioHandler.exportFrames(inputFrames);
+
+    Mat i1, i2, out;
+    Mat xy[2];
+    Mat vx;
+    Mat vy;
+    for (int i = 0; i < inputFrames.size() - 1; i++) {
+        i1 = qimage_to_mat_ref(inputFrames.at(i), CV_8UC3);
+        i2 = qimage_to_mat_ref(inputFrames.at(i + 1), CV_8UC3);
+        std::cout << "flow #" << to_string(i) <<  " calculated" << std::endl;
+        out = calculateFlow(i1, i2);
+        std::cout << "mat type: " << type2str(out.type()) << std::endl;
+        split(out, xy);
+        convertScaleAbs(xy[0],vx, 100, 0.0);
+        convertScaleAbs(xy[1],vy, 100, 0.0);
+//        imshow("flow vx", vx);
+//        imshow("flow vy", vy);
+//        waitKey(0);
+//        out.convertTo(out, CV_8UC3, 255.0);
+        imwrite(outputDir.toStdString() + "/flow_vx" + to_string(i) +  ".jpg", vx);
+        imwrite(outputDir.toStdString() + "/flow_vy" + to_string(i) +  ".jpg", vy);
+
+    }
+
+
 
     a.exit();
+
+
 }
+
+
