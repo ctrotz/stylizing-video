a3f47930d1a016adc27ba3345db9d289b3d5d265
Isa Milefchik <isavmilefchik@gmail.com>
2020-04-22 18:04:31 -0700

    GTemp implemented

--- src/gtemp.cpp
+++ src/gtemp.cpp
@@ -1,20 +1,31 @@
 #include "gtemp.h"
+#include "advector.h"
 
-GTemp::GTemp(std::shared_ptr<QImage> prevFrame, std::shared_ptr<QImage> currFrame, std::shared_ptr<QImage> keyFrame, std::shared_ptr<QImage> motionField, std::shared_ptr<QImage> prevOut) :
+GTemp::GTemp(std::shared_ptr<QImage> currFrame,
+             std::shared_ptr<QImage> mask,
+             cv::Mat& motionField,
+             std::shared_ptr<QImage> prevOut) :
     Guide(currFrame),
     m_guide(nullptr)
 {
-    createTemp();
+    createTemp(motionField, mask, prevOut);
 }
 
-GTemp::~GTemp(){
+GTemp::~GTemp()
+{
     m_guide = nullptr;
 }
 
-std::shared_ptr<QImage> GTemp::getGuide(){
+std::shared_ptr<QImage> GTemp::getGuide()
+{
     return m_guide;
 }
 
-void GTemp::createTemp(){
-
+void GTemp::createTemp(cv::Mat& motionField, std::shared_ptr<QImage> mask,
+                       std::shared_ptr<QImage> prevFrame)
+{
+    Advector advector = Advector();
+    std::shared_ptr<QImage> newFrame(new QImage(*prevFrame));
+    advector.advect(motionField, mask, prevFrame, newFrame);
+    m_guide = newFrame;
 }
--- src/gtemp.h
+++ src/gtemp.h
@@ -3,19 +3,22 @@
 
 #include "guide.h"
 #include <QObject>
+#include <opencv2/core/mat.hpp>
 
 class GTemp : public Guide
 {
 public:
-    GTemp(std::shared_ptr<QImage> prevFrame, std::shared_ptr<QImage> currFrame, std::shared_ptr<QImage> keyFrame, std::shared_ptr<QImage> motionField, std::shared_ptr<QImage> prevOut);
+    GTemp(std::shared_ptr<QImage> currFrame,
+          std::shared_ptr<QImage> mask,
+          cv::Mat& motionField,
+          std::shared_ptr<QImage> prevOut);
     virtual ~GTemp();
-
-protected:
     std::shared_ptr<QImage> getGuide();
 
 private:
     std::shared_ptr<QImage> m_guide;
-    void createTemp();
+    void createTemp(cv::Mat& motionField, std::shared_ptr<QImage> mask,
+                    std::shared_ptr<QImage> prevFrame);
 };
 
 #endif // GTEMP_H
